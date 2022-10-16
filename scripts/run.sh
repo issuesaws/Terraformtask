a9b865c50a92f8c15971d2d0158c33a2ac3bccba
issuesaws <issuesaws@gmail.com>
2022-10-16 16:04:41 +0530

    Added changes

---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+sudo mv springpetclinic.service /etc/systemd/system/springpetclinic.service
+sudo chmod 755 springpetclinic.sh javamaveninstallation.sh startservice.sh
+sudo sh javamaveninstallation.sh
+sudo sh springpetclinic.sh
+sudo sh startservice.sh
+sudo sh exit
